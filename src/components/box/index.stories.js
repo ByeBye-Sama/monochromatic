d240913888ac432d06aa378140d3c20fe8342f49
ByeBye-Sama <joaquin.byebye@gmail.com>
2020-05-15 01:49:06 -0500

    feat: add center box prop & gradiant text

--- src/components/box/index.stories.js
+++ src/components/box/index.stories.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { keys } from 'lodash'
 import styled from 'styled-components'
-import { text, select, boolean } from '@storybook/addon-knobs'
+import { readableColor } from 'polished'
+import { text, select, boolean, object } from '@storybook/addon-knobs'
 import { theme } from 'theme'
 import { Typography } from 'components'
 import Box from './index'
@@ -11,37 +12,56 @@ const metadata = {
 }
 
 const StyledBox = styled(Box)`
-  min-height: ${theme.spacing(12)};
+  min-height: ${theme.spacing(15)};
+  padding: ${theme.spacing(0, 2)};
+  width: calc(100% - ${theme.spacing(4)});
 `
 
 export const Normal = () => {
-  const value = text('value', 'Placeholder')
-
-  const display = text('display', 'flex')
-
-  const justifyContent = text('justifyContent', 'center')
-
-  const alignItems = text('alignItems', 'center')
-
   const background = select('background', keys(theme.palette), 'shade3')
+
+  const center = boolean('center', false)
 
   const rounded = boolean('rounded', false)
 
-  const color = select('color', keys(theme.palette), 'tint2')
+  const display = text('display', 'flex')
+
+  const justifyContent = text('justifyContent', 'space-between')
+
+  const alignItems = text('alignItems', 'flex-start')
+
+  const value = text('text', 'Placeholder')
+
+  const color = readableColor(theme.palette[background])
+
+  const textColor = color === '#fff' ? 'white' : 'black'
 
   return (
     <StyledBox
+      center={center}
       display={display}
       background={background}
       alignItems={alignItems}
       justifyContent={justifyContent}
       rounded={rounded}
     >
-      <Typography variant="h1" color={color}>
+      <Typography variant="h1" color={textColor}>
         {value}
       </Typography>
     </StyledBox>
   )
+}
+
+export const Gradient = () => {
+  const defaultValue = {
+    from: 'shade3',
+    to: 'primary00',
+    direction: 'right bottom'
+  }
+
+  const background = object('background', defaultValue)
+
+  return <StyledBox background={background} />
 }
 
 export default metadata
--- src/components/typography/index.js
+++ src/components/typography/index.js
@@ -1,10 +1,21 @@
 import styled from 'styled-components'
+import { isPlainObject } from 'lodash'
 import { theme } from 'theme'
 
 const resolveColor = props => {
   const { color } = props
 
-  return theme.palette[color || 'shade3']
+  if (isPlainObject(color)) {
+    const { from, to, direction } = color
+
+    return `
+      background: linear-gradient(to ${direction}, ${theme.palette[from]} 0%, ${theme.palette[to]} 100%);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    `
+  }
+
+  return `color: ${theme.palette[color || 'shade3']};`
 }
 
 const resolveVariant = props => {
@@ -27,10 +38,9 @@ const resolveAlign = props => {
 }
 
 const Typography = styled.div`
-  color: ${resolveColor};
   display: inline-block;
   text-decoration: none;
-
+  ${resolveColor}
   ${resolveAlign}
   ${resolveVariant}
 `
